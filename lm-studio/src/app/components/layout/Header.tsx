e11c27d741e9775c5039f1fbd9389f64c6dd9373
Roberto <san20086@byui.edu>
2025-01-09 19:56:29 -0500

    Small change in header color font.

--- lm-studio/src/app/components/layout/Header.tsx
+++ lm-studio/src/app/components/layout/Header.tsx
@@ -3,18 +3,17 @@ import { ShoppingCart } from "lucide-react";
 
 export const Header: FC = () => {
   return (
-    <header className="py-6 flex justify-between items-center text-black">
-      <nav>
-        <ul className="flex space-x-6">
-          <li><a href="#appointments" className="hover:underline">Appointments</a></li>
-          <li><a href="#shop" className="hover:underline">Shop</a></li>
-        </ul>
-      </nav>
-      <h1 className="text-4xl font-bold">LM Studio</h1>
-      <div>
-        <ShoppingCart className="h-6 w-6" />
-      </div>
-    </header>
+    <header className="py-6 flex justify-between items-center text-white"> 
+    <nav> 
+      <ul className="flex space-x-6"> 
+        <li><a href="#appointments" className="hover:underline text-">Appointments</a></li> 
+        <li><a href="#shop" className="hover:underline">Shop</a></li> 
+      </ul> 
+    </nav> 
+    <h1 className="text-4xl font-bold text-blue-500">LM Studio</h1> 
+    <div> <ShoppingCart className="h-6 w-6" /> 
+    </div> 
+    </header> 
   );
 };
 
